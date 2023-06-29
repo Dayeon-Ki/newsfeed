d8c39edb9ca3717fb1e1763ce340d4e04f58807e
Kim Jinkyung <jinkyung127@naver.com>
2023-06-29 11:58:00 +0900

    게시글페이지 게시글 수정

--- public/postDetail.js
+++ public/postDetail.js
@@ -2,6 +2,7 @@
 const params = new URLSearchParams(window.location.search);
 const postId = params.get('id');
 
+// 게시글 상세 정보 조회 api 요청
 window.addEventListener('DOMContentLoaded', function () {
   console.log(postId);
 
@@ -10,12 +11,57 @@ window.addEventListener('DOMContentLoaded', function () {
     .then(data => {
       console.log(data);
 
-      const postTitle = document.querySelector('#postdetailBox h3');
-      postTitle.textContent = '게시글 제목: ' + data.post.title;
+      const postTitle = document.querySelector('.postTitle');
+      postTitle.textContent = data.post.title;
       const postWriter = document.querySelector('#writer p');
-      postWriter.textContent = '작성자: ' + data.post.user.nickname;
+      postWriter.textContent = data.post.user.nickname;
       const postContent = document.querySelector('.postContent');
       postContent.textContent = data.post.content;
+
+      let postTitleEdit = document.querySelector('#modifyTitle');
+      postTitleEdit.value = data.post.title;
+      let postContentEdit = document.querySelector('#modifyContent');
+      postContentEdit.value = data.post.content;
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
+});
+
+// 모달 창 열기
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = 'block';
+}
+
+// 모달 창 닫기
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = 'none';
+}
+
+// 수정 버튼을 누르면 수정 모달 띄우기
+document.getElementById('postMdBtn').addEventListener('click', function () {
+  openModal('modifyModal');
+});
+
+// 확인 버튼 클릭 시 게시글 수정 api 요청
+document.getElementById('modifySubmit').addEventListener('click', async function () {
+  const modifiedTitle = document.getElementById('modifyTitle').value;
+  const modifiedContent = document.querySelector('#modifyContent').value;
+
+  fetch(`/api/posts/${postId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title: modifiedTitle, content: modifiedContent }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      alert('게시글이 수정되었습니다');
+      location.reload();
     })
     .catch(error => {
       console.log('error', error);
