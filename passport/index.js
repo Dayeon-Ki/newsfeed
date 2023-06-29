ea70f03e8bcac66f1b7d525daec96be5efe2fd65
byeongsoo52 <132368027+byeongsoo52@users.noreply.github.com>
2023-06-29 21:48:23 +0900

    카카오 로그인 에러 해결

--- passport/index.js
+++ passport/index.js
@@ -1,5 +1,4 @@
 const passport = require('passport');
-const local = require('./localStrategy'); // 로컬서버로 로그인할때
 const kakao = require('./kakaoStrategy'); // 카카오서버로 로그인할때
 
 const User = require('../models/user');
@@ -17,6 +16,5 @@ module.exports = () => {
          .catch(err => done(err));
    }); 
 
-   local();
-   kakao(); // 구글 전략 등록
+   kakao(); // 카카오 전략 등록
 };
